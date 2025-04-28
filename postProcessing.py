8cfdb407a258bb8b874169b6539c4497f6dce64a
dgeneau <dgeneau@csipacific.ca>
2025-04-28 15:00:34 -0700

    Update postProcessing.py

--- postProcessing.py
+++ postProcessing.py
@@ -46,7 +46,7 @@ with title:
 CLIENT_ID = "9b430ac6-b8e8-475e-9d9c-c99484b3535d"
 TENANT_ID = "0f3fdf7c-bc2c-4ba0-9ae4-14b4718b01e7"
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-REDIRECT_URI = "http://localhost:8501/"
+REDIRECT_URI = "https://gpspostprocesssing-tqnxe5mzjzk5eu2rf8gbmq.streamlit.app/"
 AUTH_ENDPOINT = f"{AUTHORITY}/oauth2/v2.0/authorize"
 TOKEN_ENDPOINT = f"{AUTHORITY}/oauth2/v2.0/token"
 SCOPES = ["https://graph.microsoft.com/Files.Read.All"]
